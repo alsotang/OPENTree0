b850cdc44108d25c6cb749885bc63df731ce9752
alsotang <alsotang@gmail.com>
2012-05-06 00:41:41 +0800

    整理了目录结构

--- new.sql
+++ new.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `account_info` (
   `oauth_token_secret` varchar(6) NOT NULL,
   `join_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `total_tweets` int(10) unsigned NOT NULL DEFAULT '0',
-  `dm_since_id` varchar(15) NOT NULL,
+  `rt_since_id` varchar(15) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS `account_info` (
 -- 表的结构 `tweets`
 --
 
-CREATE TABLE IF NOT EXISTS `tweets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `local_id` int(11) NOT NULL DEFAULT '0',
-  `tweet_id` varchar(15) NOT NULL,
-  `from_whom` varchar(15) NOT NULL,
-  `from_tree0` varchar(15) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+--CREATE TABLE IF NOT EXISTS `tweets` (
+--  `id` int(11) NOT NULL AUTO_INCREMENT,
+--  `local_id` int(11) NOT NULL DEFAULT '0',
+--  `tweet_id` varchar(15) NOT NULL,
+--  `from_whom` varchar(15) NOT NULL,
+--  `from_tree0` varchar(15) NOT NULL,
+--  PRIMARY KEY (`id`)
+--) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 
