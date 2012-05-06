c8250613e8ab841128ae92b9411e14ae31fce611
alsotang <alsotang@gmail.com>
2012-05-06 15:03:07 +0800

    合并了“无需转发微博”的检测方式，通过一个`forbidden_tweet'函数。

--- sae/cronjob.py
+++ sae/cronjob.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # coding=utf-8
 import urllib2
+import logging
 
 from weibo1 import APIClient, OAuthToken
 
@@ -24,12 +25,12 @@ class cronjob():
         if not rts:
             return False
         else:
-            for i in reversed(rts): 
-                if i.user.id == 1468317623:
+            for rt in reversed(rts): 
+                #print '~~~~%s~~~~' % i
+                if self.forbidden_tweet(rt): #不符合转发条件的
                     continue
-                
                 try:
-                    self.parse_rt(i)
+                    self.parse_rt(rt)
                     return True # 每次循环只转发一次就好了
                 except urllib2.HTTPError:
                     continue
@@ -40,8 +41,38 @@ class cronjob():
         return rts
 
     def parse_rt(self, rt):
-        self.client.post.statuses__repost(id=rt.id, status='//@%s: %s' % (rt.user.screen_name, rt.text.replace(u'''//@广西北海中学: ''', '')))
+        self.client.post.statuses__repost(id=rt.id, status='//@%s: %s' % (rt.user.screen_name, rt.text.replace(u'''//@广西北海中学:''', '')))
         self.since_id = rt.id
+
+    def forbidden_tweet(self, rt):
+        def is_too_many_retweets(rt):
+            try:
+                tid = str(rt.retweeted_status.id)
+            except KeyError:
+                logging.debug('-'*80 + 'KeyError' + rt.text)
+                tid = str(rt.id)
+            #print '----%s----%s----%s' % (rt, rt.retweeted_status.id, (tid))
+            times = cache.get(tid)
+            if times:
+                logging.info("%s: %s times %s" % (tid, times, rt.retweeted_status.text))
+                if times > 20:
+                    return True
+                else:
+                    cache.incr(tid)
+            else:
+                cache.set(tid, '1')
+            return False
+
+        def no_comment(rt):
+            if rt.text.startswith('//'):
+                return True
+
+        if rt.user.id == 1468317623 or is_too_many_retweets(rt) or no_comment(rt): #不符合转发条件的
+            return True
+        else:
+            return False
+
+
 
 
 if __name__ == "__main__":
